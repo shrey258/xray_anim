d317a6c2bc2482401c4f8c96f7666bd4335df7f3
Shreyansh Gupta <hardikshrey123@gmail.com>
2025-12-10 01:31:34 +0530

    Add mouse-tracking circular reveal effect for Spider-Man image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import { useEffect, useState, type CSSProperties } from 'react';
 import './App.css'
 
 function App() {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      setCoords({
+        x: event.clientX,
+        y: event.clientY
+      });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  const dynamicStyles = {
+    '--mouse-x': `${coords.x}px`,
+    '--mouse-y': `${coords.y}px`,
+  } as CSSProperties;
+
   return (
-    <div className="image-container">
-      <img src="/spiderman.png" alt="Spiderman" className="layer-image" />
-      <img src="/miles.png" alt="Miles" className="layer-image" />
+    <div className="image-container" style={dynamicStyles}>
+      <img src="/miles.png" alt="Miles" className="miles-image" />
+      <img src="/spiderman.png" alt="Spiderman" className="spider-image" />
+      
     </div>
   )
 }
