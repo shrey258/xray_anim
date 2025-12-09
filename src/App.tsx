4e78f7f54e9aa6e3e68856b474a37fa0ed24d2da
Shreyansh Gupta <hardikshrey123@gmail.com>
2025-12-09 23:34:24 +0530

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="image-container">
+      <img src="/spiderman.png" alt="Spiderman" className="layer-image" />
+      <img src="/miles.png" alt="Miles" className="layer-image" />
+    </div>
+  )
+}
+
+export default App
